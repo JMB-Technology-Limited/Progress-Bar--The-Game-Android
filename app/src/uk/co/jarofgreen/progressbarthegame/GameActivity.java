fb55f60f89e84affd0f1239eb71e2c25c87da150
James <james@jarofgreen.co.uk>
2013-04-20 19:55:49 +0100

    Start game activity

--- app/src/uk/co/jarofgreen/progressbarthegame/GameActivity.java
+++ app/src/uk/co/jarofgreen/progressbarthegame/GameActivity.java
@@ -2,9 +2,7 @@ package uk.co.jarofgreen.progressbarthegame;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
 
 /**
@@ -14,23 +12,12 @@ import android.view.View;
  * @license Open Source under the 3-clause BSD License
  * @url https://github.com/jarofgreen/Progress-Bar--The-Game-Android
  *  */
-public class SplashActivity extends Activity {
+public class GameActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash, menu);
-		return true;
-	}
-	
-	public void start(View view) {
-		startActivity(new Intent(this, GameActivity.class));
+		setContentView(R.layout.activity_game);
 	}
 
 }
